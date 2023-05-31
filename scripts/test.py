d2d7b91336700623ab0cfabae4f249cf0abcabd1
uriuriboo <rokkouribou@gmail.com>
2023-05-31 07:11:31 +0000

    taskrunnerを使ってクリックでビルドとテストを実行できるようにした。

---
+++ scripts/test.py
@@ -0,0 +1,19 @@
+import os,sys
+import subprocess
+from urllib.request import urlopen
+
+def get_contest_id(path) -> tuple:
+    parsed_path = path.split("/")
+    contest_name = parsed_path[3][:3]
+    contest_id = parsed_path[3][3:]
+    problem_id = parsed_path[6][0]
+    return contest_name,contest_id,problem_id
+
+def download_test(contest_name,contest_id,problem_id):
+    url = "https://atcoder.jp/contests/{0}/tasks/{0}_{1}".format(contest_id,problem_id)
+    # cp = subprocess.run("pwd") /workspaces/atcoder_rust
+    # cp = subprocess.run(["./bt.bash",contest_name,contest_id,problem_id])
+    cp = subprocess.run(["./bt.bash",contest_name,contest_id,problem_id])
+
+contest_name,contest_id,problem_id = get_contest_id(sys.argv[1])
+download_test(contest_name,contest_id,problem_id)
