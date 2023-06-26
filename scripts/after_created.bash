51cf9ca543fc0d5db6ce1e7694e33c44d7a5147c
uriuriboo <rokkouribou@gmail.com>
2023-06-26 10:44:06 +0000

    rust-analyzerを使いたい場合のコマンドをコメントアウトで追加

--- scripts/after_created.bash
+++ scripts/after_created.bash
@@ -3,3 +3,9 @@ echo "1.42" > rust-toolchain
 cargo -V
 rustup install 1.42
 rustup install 1.42.0-x86_64-unknown-linux-gnu
+
+# if you want to use rust-analyzer ,use rust-toolcain 1.65 or later
+# echo "1.70" > rust-toolchain
+# cargo -V
+# rustup install 1.70
+# rustup install 1.70.0-x86_64-unknown-linux-gnu
