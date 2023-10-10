e788efbe3ba442a874eadfa93b3a02c8ec85feda
uriboo <rokkouribou@gmail.com>
2023-10-10 03:04:46 +0000

    バージョン変更、権限付与時の修正

--- .devcontainer/postCreateCommand.bash
+++ .devcontainer/postCreateCommand.bash
@@ -4,23 +4,34 @@ if [ -e rust-toolchain ];then
     rm rust-toolchain
 fi
 echo give permission to bashfiles
-chmod a+x bt.bash,sub.bash,scripts/bt.bash,scripts/sub.bash,scripts/change_launch.bash
-echo install fish and fisher
-sudo apt-add-repository ppa:fish-shell/release-3
-sudo apt-get update
-sudo apt-get install fish
-curl -sL https://git.io/fisher | source && fisher install jorgebucaran/fisher
+chmod u+x bt.bash,sub.bash,scripts/bt.bash,scripts/sub.bash,scripts/change_launch.bash
+
+# install rust1.70.0
+echo "1.70" > rust-toolchain
+cargo -V
+
+touch Cargo.toml
+
+# install tools
 cargo install cargo-compete
 cargo install cargo-member
 cargo compete i atcoder
-echo login to atcoder
-cargo compete l atcoder
 echo clone ac-library-rs
 git clone https://github.com/rust-lang-ja/ac-library-rs.git
 cargo member include ac-library-rs
-cargo member include library
-# touch rust-toolchain
-# echo "1.42" > rust-toolchain
-# cargo -V
-# rustup install 1.42
-# rustup install 1.42.0-x86_64-unknown-linux-gnu
+# cargo member include library
+
+echo login to atcoder
+cargo compete l atcoder
+
+
+# rustup install 1.70
+# rustup install 1.70.0-x86_64-unknown-linux-gnu
+
+# The installation of fish is your choice.
+# echo install fish and fisher
+# sudo apt-add-repository ppa:fish-shell/release-3
+# sudo apt-get update
+# sudo apt-get install fish
+# curl -sL https://git.io/fisher | source && fisher install jorgebucaran/fisher
+
