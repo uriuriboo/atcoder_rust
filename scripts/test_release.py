56e3691d6868e5ff8f064ac0426444916a7796a4
uriboo <rokkouribou@gmail.com>
2023-10-21 05:16:29 +0000

    リリースコマンドをつけてコンパイルするtaskを追加

---
+++ scripts/test_release.py
@@ -0,0 +1,17 @@
+import os,sys
+import subprocess
+from urllib.request import urlopen
+
+def get_contest_id(path) -> tuple:
+    parsed_path = path.split("/")
+    contest_name = parsed_path[3][:3]
+    contest_id = parsed_path[3][3:]
+    problem_id = parsed_path[6][0]
+    return contest_name,contest_id,problem_id
+
+
+contest_name,contest_id,problem_id = get_contest_id(sys.argv[1])
+path = '/workspaces/atcoder_rust/{}{}/src/bin'.format(contest_name,contest_id)
+os.chdir(path)
+cp = subprocess.run(["cargo" ,"build"])
+cp = subprocess.run(["cargo" ,"compete" ,"t" ,problem_id ,"--release"])
